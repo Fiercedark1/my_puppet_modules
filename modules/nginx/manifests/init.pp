73f021e22e29e943d5fcd0280b4fac694ae05769
Fiercedark1 <ecarra7979@gmail.com>
2016-05-16 18:14:28 -0400

    Puupet hierarchy tree and the begining of nginx service and file templates

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,16 @@
+#Class: nginx
+#
+#This module manages the installation and configuration of nginx
+#
+#Author: Edwin Carrasquillo
+#
+# Date: May 16, 2016
+#
+# Requires: default.conf in the files directory, and an IP once configuration is ready.
+#
+# Usage: To install nginx as a webserver for hosting a website. This is currently set to pull a default.conf file where the IP is not yet specified.
+#
+# 
+class nginx {
+
+}
