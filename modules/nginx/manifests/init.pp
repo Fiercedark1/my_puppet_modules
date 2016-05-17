fde393ef0cd9830abc71240ce1b807317c55428a
Fiercedark1 <ecarra7979@gmail.com>
2016-05-17 17:12:27 -0400

    completed nginx init.pp: handles install, server, default.conf config

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -12,5 +12,27 @@
 #
 # 
 class nginx {
+  package { 'nginx':
+    ensure    => installed,
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0644",
+}
+  File {
+    ensure    => file,
+    owner     => 'root',
+    group     => 'root',
+    mode      => '0600',
+}
 
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['default.conf'],
+}
+
+  file { 'default.conf':
+    path      => '/etc/nginx/conf.d/default.conf',
+    require   => Package['nginx'],
+    source    => "puppet:///modules/nginx/default.conf"
 }
